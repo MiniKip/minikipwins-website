c6963e0027183aef4c791861d23579318edfa984
MiniKip <Minikip@proton.me>
2026-06-13 16:10:24 +0600

    Delete server.py

--- server.py
+++
@@ -1,9 +0,0 @@
-from http.server import HTTPServer, SimpleHTTPRequestHandler
-
-host = "0.0.0.0"
-port = 6969
-
-print(f"🦐 MiniKip server running at http://{host}:{port}")
-print("close with Ctrl+C")
-
-HTTPServer((host, port), SimpleHTTPRequestHandler).serve_forever()
